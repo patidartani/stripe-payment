fbcd8b29b4e5595b1f5daa4faa90d9e6bbcc5c66
Tanisha Patidar <tanishapatidar905@gmail.com>
2025-07-04 10:52:22 +0530

    Final MERN (CRA) app for Railway

--- server/app.js
+++ server/app.js
@@ -1,15 +1,46 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const dotenv=require("dotenv");
-dotenv.config()
-const connectDb=require("./Database/database")
-connectDb()
-const router=require("./routes/router")
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config();
+
+const connectDb = require("./Database/database");
+connectDb();
+
+const router = require("./routes/router");
+
 app.use(express.json());
 app.use(cors());
 
-//checkout api
-app.use(router)
+// API Routes
+app.use(router);
 
-module.exports =app;
+// ========= Serve Frontend (  Build) =========
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+// ================================================
+
+module.exports = app;
+
+
+
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const dotenv=require("dotenv");
+// dotenv.config()
+// const connectDb=require("./Database/database")
+// connectDb()
+// const router=require("./routes/router")
+// app.use(express.json());
+// app.use(cors());
+
+// //checkout api
+// app.use(router)
+
+// module.exports =app;
